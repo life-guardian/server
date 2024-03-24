b8bb8ba0fe5104f3901c9496550fc56919c10ad1
Pratik Patil <pratik8560@gmail.com>
2024-03-24 20:46:44 +0530

    Updated initialConnect

--- controllers/rescueOps.controller.js
+++ controllers/rescueOps.controller.js
@@ -151,13 +151,49 @@ const agencyOnInitialConnect = async (req, res) => {
 
 const rescueMe = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
+    await User.findByIdAndUpdate(req.user.id, {
+      $set: { "rescue.isInDanger": true, "rescue.reason": req.body.rescueReason },
+    });
 
-    res.status(200).json({});
+    res.status(200).json({ message: "Rescue me!" });
   } catch (error) {
-    console.error(`Error fetching onGoingRescueOperation status: ${error}`);
-    return res.status(500).json({ message: "Error fetching onGoingRescueOperation status" });
+    console.error(`Error changing rescueme status: ${error}`);
+    return res.status(500).json({ message: "Error changing rescueme status" });
   }
 };
 
-module.exports = { startRescueOps, deleteRescueOps, stopRescueOps, isRescueOperationOnGoing, agencyOnInitialConnect };
+//first check before connecting calling rescueMe api if the user is already indanger and if his app was closed and now he has reopened it then check if the rescueMealreadystarted and if not then call the rescueMe api and connect to socket else directly connectToSocket
+const isAlreadyRescueMeStarted = async (req, res) => {
+  try {
+    const user = await User.findbyId(req.user.id);
+    return res.status(200).json({ userAlreadyInDanger: user.rescue.isInDanger });
+  } catch (error) {
+    console.error(`Error fetching isAlreadyRescueMeStarted: ${error}`);
+    return res.status(500).json({ message: "Error fetching isAlreadyRescueMeStarted" });
+  }
+};
+
+//stop after rescueMe
+const stopUserRescue = async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $set: { "rescue.isInDanger": false, "rescue.reason": null },
+    });
+
+    res.status(200).json({ message: "Stopped user rescue" });
+  } catch (error) {
+    console.error(`Error changing stop rescueme status: ${error}`);
+    return res.status(500).json({ message: "Error changing stop rescueme status" });
+  }
+};
+
+module.exports = {
+  startRescueOps,
+  deleteRescueOps,
+  stopRescueOps,
+  isRescueOperationOnGoing,
+  agencyOnInitialConnect,
+  rescueMe,
+  stopUserRescue,
+  isAlreadyRescueMeStarted,
+};
--- socket/socketFunctions.js
+++ socket/socketFunctions.js
@@ -16,6 +16,31 @@ const handleOnConnection = async (socket) => {
 };
 
 const handleOnInitialConnect = async (socket, locationPayload) => {
+  const nearbyAgencies = await fetchNearest(Agency, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
+  const agencySocketIds = nearbyAgencies.filter((agency) => agency.socketId).map((agency) => agency.socketId);
+
+  // Fetch nearby agencies
+
+  const populatedAgencies = await Agency.populate(nearbyAgencies, { path: "onGoingRescueOperation" });
+
+  const agencies = populatedAgencies
+    .filter((agency) => agency.socketId && agency._id.toString() !== socket.user.id.toString()) // Filter out own user's data
+    .map((agency) => {
+      return {
+        lng: agency.lastLocation.coordinates[0],
+        lat: agency.lastLocation.coordinates[1],
+        agencyId: agency._id,
+        agencyName: agency.name,
+        phoneNumber: agency.phone,
+        representativeName: agency.representativeName,
+        rescueOpsName: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.name : null,
+        rescueOpsDescription: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.description : null,
+        rescueTeamSize: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.rescueTeamSize : null,
+      };
+    });
+
+  socket.emit("initialConnectReceiveNearbyAgencies", agencies);
+
   // Fetch nearby users if the socket user is an agency
   if (socket.user.isAgency) {
     const nearbyUsers = await fetchNearest(User, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
@@ -29,34 +54,78 @@ const handleOnInitialConnect = async (socket, locationPayload) => {
           userId: user._id,
           userName: user.name,
           phoneNumber: user.phoneNumber,
+          isInDanger: user.rescue.isInDanger,
+          rescueReason: user.rescue.reason,
         };
       });
     socket.emit("initialConnectReceiveNearbyUsers", users);
+
+    const agencyLocation = {
+      type: "Point",
+      coordinates: [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)],
+    };
+
+    const result = await Agency.findOneAndUpdate({ _id: socket.user.id }, { $set: { lastLocation: agencyLocation } });
+    if (!result) {
+      socket.emit("agencyLocationUpdate", {
+        status: false,
+        message: "You as agency not found",
+      });
+      return;
+    }
+
+    const agencyData = await Agency.findById(socket.user.id).populate("onGoingRescueOperation");
+
+    const currentAgencyData = {
+      ...locationPayload,
+      agencyId: agencyData._id,
+      agencyName: agencyData.name,
+      phoneNumber: agencyData.phone,
+      representativeName: agencyData.representativeName,
+      rescueOpsName: agencyData.onGoingRescueOperation ? agencyData.onGoingRescueOperation.name : null,
+      rescueOpsDescription: agencyData.onGoingRescueOperation ? agencyData.onGoingRescueOperation.description : null,
+      rescueTeamSize: agencyData.onGoingRescueOperation ? agencyData.onGoingRescueOperation.rescueTeamSize : null,
+    };
+
+    const userSocketIds = nearbyUsers.filter((user) => user.socketId).map((user) => user.socketId);
+    if (userSocketIds.length > 0) {
+      socket.broadcast.to(userSocketIds).emit("agencyLocationUpdate", currentAgencyData);
+    }
+
+    if (agencySocketIds.length > 0) {
+      socket.broadcast.to(agencySocketIds).emit("agencyLocationUpdate", currentAgencyData);
+    }
+
+    console.log(
+      `\nInitially connected agency ${socket.id} and the data is \n${JSON.stringify(agencies)}\n${JSON.stringify(
+        agencies
+      )}\n`
+    );
+  } else {
+    const userLocation = {
+      type: "Point",
+      coordinates: [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)],
+    };
+    const user = await User.findOneAndUpdate(
+      { _id: socket.user.id },
+      { $set: { lastLocation: userLocation } },
+      { new: true }
+    );
+
+    const currentUserData = {
+      ...locationPayload,
+      userId: user._id,
+      userName: user.name,
+      phoneNumber: user.phoneNumber,
+      isInDanger: user.rescue.isInDanger,
+      rescueReason: user.rescue.reason,
+    };
+
+    if (agencySocketIds.length > 0) {
+      socket.broadcast.to(agencySocketIds).emit("userLocationUpdate", currentUserData);
+    }
+    console.log(`\nInitially connected user ${socket.id} and the data is \n${JSON.stringify(agencies)}\n`);
   }
-
-  // Fetch nearby agencies
-  const nearbyAgencies = await fetchNearest(Agency, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
-
-  const populatedAgencies = await Agency.populate(nearbyAgencies, { path: "onGoingRescueOperation" });
-
-  const agencies = populatedAgencies
-    .filter((agency) => agency.socketId && agency._id.toString() !== socket.user.id.toString()) // Filter out own user's data
-    .map((agency) => {
-      return {
-        lng: agency.lastLocation.coordinates[0],
-        lat: agency.lastLocation.coordinates[1],
-        agencyId: socket.user.id,
-        agencyName: agency.name,
-        phoneNumber: agency.phone,
-        representativeName: agency.representativeName,
-        rescueOpsName: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.name : null,
-        rescueOpsDescription: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.description : null,
-        rescueTeamSize: agency.onGoingRescueOperation ? agency.onGoingRescueOperation.rescueTeamSize : null,
-      };
-    });
-
-  socket.emit("initialConnectReceiveNearbyAgencies", agencies);
-  console.log(`Initially connected user ${socket.id} and the data is ${JSON.stringify(agencies)}\n`);
 };
 
 // Throttle the userLocationUpdate event
@@ -90,21 +159,24 @@ const handleUserLocationUpdate = _.throttle(async (socket, locationPayload) => {
   const nearbyAgencies = await fetchNearest(Agency, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
 
   //filtering agencies whose socketId is null. This means they have not joined socket connection for their location sharing
-  const socketIds = nearbyAgencies.filter((agency) => agency.socketId).map((agency) => agency.socketId);
+  const agencySocketIds = nearbyAgencies.filter((agency) => agency.socketId).map((agency) => agency.socketId);
 
   const responseData = {
     ...locationPayload,
-    userId: socket.user.id,
+    userId: user._id,
     userName: user.name,
     phoneNumber: user.phoneNumber,
+    isInDanger: user.rescue.isInDanger,
+    rescueReason: user.rescue.reason,
   };
 
-  if (socketIds.length > 0) {
-    socket.broadcast.to(socketIds).emit("userLocationUpdate", responseData);
+  if (agencySocketIds.length > 0) {
+    socket.broadcast.to(agencySocketIds).emit("userLocationUpdate", responseData);
   }
+
+  console.log(`Processed user update of : ${socket.id} user name: ${user.name}`);
 }, THROTTLE_INTERVAL);
 
-// Throttle the agencyLocationUpdate event
 const handleAgencyLocationUpdate = _.throttle(async (socket, locationPayload, updateId) => {
   if (!socket.user.isAgency) {
     socket.emit("agencyLocationUpdate", {
@@ -127,7 +199,6 @@ const handleAgencyLocationUpdate = _.throttle(async (socket, locationPayload, up
     });
     return;
   }
-  const agencyData = await Agency.findById(socket.user.id).populate("onGoingRescueOperation");
 
   const nearbyAgencies = await fetchNearest(Agency, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
 
@@ -139,9 +210,11 @@ const handleAgencyLocationUpdate = _.throttle(async (socket, locationPayload, up
 
   const userSocketIds = nearbyusers.filter((user) => user.socketId).map((user) => user.socketId);
 
+  const agencyData = await Agency.findById(socket.user.id).populate("onGoingRescueOperation");
+
   const responseData = {
     ...locationPayload,
-    agencyId: socket.user.id,
+    agencyId: agencyData._id,
     agencyName: agencyData.name,
     phoneNumber: agencyData.phone,
     representativeName: agencyData.representativeName,
@@ -157,7 +230,7 @@ const handleAgencyLocationUpdate = _.throttle(async (socket, locationPayload, up
   if (userSocketIds.length > 0) {
     socket.broadcast.to(userSocketIds).emit("agencyLocationUpdate", responseData);
   }
-  console.log(`Processed update of : ${socket.id} and update id: ${updateId} agency name: ${agencyData.name}`);
+  console.log(`Processed agency update of : ${socket.id} and update id: ${updateId} agency name: ${agencyData.name}`);
 }, THROTTLE_INTERVAL);
 
 const handleDisconnect = async (socket) => {
