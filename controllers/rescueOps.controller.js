5dd2853429e3d87e8b8506cce358c80938c5e573
Pratik Patil <pratik8560@gmail.com>
2024-04-19 09:11:28 +0530

    Sending email to main office when there is no agency to rescue nearby

--- controllers/rescueOps.controller.js
+++ controllers/rescueOps.controller.js
@@ -149,43 +149,43 @@ const agencyOnInitialConnect = async (req, res) => {
   }
 };
 
-const rescueMe = async (req, res) => {
-  try {
-    await User.findByIdAndUpdate(req.user.id, {
-      $set: { "rescue.isInDanger": true, "rescue.reason": req.body.rescueReason },
-    });
+// const rescueMe = async (req, res) => {
+//   try {
+//     await User.findByIdAndUpdate(req.user.id, {
+//       $set: { "rescue.isInDanger": true, "rescue.reason": req.body.rescueReason },
+//     });
 
-    res.status(200).json({ message: "Rescue me!" });
-  } catch (error) {
-    console.error(`Error changing rescueme status: ${error}`);
-    return res.status(500).json({ message: "Error changing rescueme status" });
-  }
-};
+//     res.status(200).json({ message: "Rescue me!" });
+//   } catch (error) {
+//     console.error(`Error changing rescueme status: ${error}`);
+//     return res.status(500).json({ message: "Error changing rescueme status" });
+//   }
+// };
 
 //first check before connecting calling rescueMe api if the user is already indanger and if his app was closed and now he has reopened it then check if the rescueMealreadystarted and if not then call the rescueMe api and connect to socket else directly connectToSocket
-const isAlreadyRescueMeStarted = async (req, res) => {
-  try {
-    const user = await User.findbyId(req.user.id);
-    return res.status(200).json({ userAlreadyInDanger: user.rescue.isInDanger });
-  } catch (error) {
-    console.error(`Error fetching isAlreadyRescueMeStarted: ${error}`);
-    return res.status(500).json({ message: "Error fetching isAlreadyRescueMeStarted" });
-  }
-};
+// const isAlreadyRescueMeStarted = async (req, res) => {
+//   try {
+//     const user = await User.findbyId(req.user.id);
+//     return res.status(200).json({ userAlreadyInDanger: user.rescue.isInDanger });
+//   } catch (error) {
+//     console.error(`Error fetching isAlreadyRescueMeStarted: ${error}`);
+//     return res.status(500).json({ message: "Error fetching isAlreadyRescueMeStarted" });
+//   }
+// };
 
 //stop after rescueMe
-const stopUserRescue = async (req, res) => {
-  try {
-    await User.findByIdAndUpdate(req.user.id, {
-      $set: { "rescue.isInDanger": false, "rescue.reason": null },
-    });
+// const stopUserRescue = async (req, res) => {
+//   try {
+//     await User.findByIdAndUpdate(req.user.id, {
+//       $set: { "rescue.isInDanger": false, "rescue.reason": null },
+//     });
 
-    res.status(200).json({ message: "Stopped user rescue" });
-  } catch (error) {
-    console.error(`Error changing stop rescueme status: ${error}`);
-    return res.status(500).json({ message: "Error changing stop rescueme status" });
-  }
-};
+//     res.status(200).json({ message: "Stopped user rescue" });
+//   } catch (error) {
+//     console.error(`Error changing stop rescueme status: ${error}`);
+//     return res.status(500).json({ message: "Error changing stop rescueme status" });
+//   }
+// };
 
 module.exports = {
   startRescueOps,
@@ -193,7 +193,7 @@ module.exports = {
   stopRescueOps,
   isRescueOperationOnGoing,
   agencyOnInitialConnect,
-  rescueMe,
-  stopUserRescue,
-  isAlreadyRescueMeStarted,
+  // rescueMe,
+  // stopUserRescue,
+  // isAlreadyRescueMeStarted,
 };
