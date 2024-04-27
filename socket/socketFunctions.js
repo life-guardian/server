0c2199185d0236d90ec7681fef04a9e82463ce27
Pratik Patil <pratik8560@gmail.com>
2024-04-27 13:25:16 +0530

    updated initial socket connect

--- socket/socketFunctions.js
+++ socket/socketFunctions.js
@@ -46,7 +46,7 @@ const handleOnInitialConnect = async (socket, locationPayload) => {
     const nearbyUsers = await fetchNearest(User, [parseFloat(locationPayload.lng), parseFloat(locationPayload.lat)]);
 
     const users = nearbyUsers
-      .filter((user) => user.socketId && user._id.toString() !== socket.user.id.toString()) // Filter out own user's data
+      .filter((user) => user.socketId && user._id.toString() !== socket.user.id.toString() && user.rescue.isInDanger) // Filter out own user's data and include only users in danger
       .map((user) => {
         return {
           lng: user.lastLocation.coordinates[0],
