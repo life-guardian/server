d4d2fa8dca4da865a368fc6a4eddfa1a066377b5
Pratik Patil <pratik8560@gmail.com>
2024-06-24 12:04:35 +0530

    restart

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ message: "status is HEALTHY!" });
+  res.status(200).json({ message: "status is OK!" });
 });
 
 app.get("/*", (req, res) => {
